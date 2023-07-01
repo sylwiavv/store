3f3231d4d93acbed536a07bb92a8d24262c8cb32
sylwia <sylwiavv@gmail.com>
2023-07-01 13:17:39 +0200

    Add seo config

---
+++ next-seo.config.js
@@ -0,0 +1,14 @@
+const  title = "Nasz sklep";
+const  description = "Nasz sklep desc";
+
+const nextSeoConfig = {
+    title,
+    description,
+    openGraph:{
+        title,
+        description,
+        siteName: 'Mój skelp - Sylwia',
+    }
+}
+
+export default nextSeoConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,13 @@
 import '../styles/global.css'
 import type { AppProps } from 'next/app'
+import SEO from "../../next-seo.config"
+import {DefaultSeo} from "next-seo"
+import {Layout} from "@/components/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   // tu dodamy layout
-  return <div><p className="text-3xl font-bold underline text-orange-600"></p><Component {...pageProps} /></div>
+   return <Layout>
+     <DefaultSeo {...SEO} />
+     <p className="text-3xl font-bold underline text-orange-600"></p><Component {...pageProps} />
+   </Layout>
 }
